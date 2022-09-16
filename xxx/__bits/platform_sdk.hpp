2e513cc4efa88597dd263bf7ae4194d43c51f093
Sven Suursoho <sven@alt.ee>
2022-09-16 19:04:12 +0300

    Try to handle windows.h min/max nuisance

--- xxx/__bits/platform_sdk.hpp
+++ xxx/__bits/platform_sdk.hpp
@@ -6,16 +6,25 @@
 		#define xxx_WIN32_LEAN_AND_MEAN
 		#define WIN32_LEAN_AND_MEAN
 	#endif
+	#if !defined(NOMINMAX)
+		#define xxx_NOMINMAX
+		#define NOMINMAX
+	#endif
 	#include <windows.h>
 	#if defined(xxx_WIN32_LEAN_AND_MEAN)
 		#undef xxx_WIN32_LEAN_AND_MEAN
 		#undef WIN32_LEAN_AND_MEAN
+	#endif
+	#if defined(xxx_NOMINMAX)
+		#undef xxx_NOMINMAX
+		#undef NOMINMAX
 	#endif
 #else
 	// included from source, must be first
 	#if defined(_WIN32) || defined(_WIN64)
 		#define WIN32_LEAN_AND_MEAN
 		#define WIN32_NO_STATUS
+		#define NOMINMAX
 		#include <windows.h>
 		#undef WIN32_NO_STATUS
 		#include <winternl.h>
